0cab8eb7fdafcc7689d53cdc1055bbbc3c018691
Mike Garrity <mike@rustytriangles.com>
2020-02-07 13:00:39 -0500

    Initial outline of teapot demo

---
+++ src/main.rs
@@ -0,0 +1,224 @@
+#[path = "teapot.rs"]
+mod teapot;
+
+use zerocopy::{AsBytes};
+
+const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0,-1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.0,
+    0.0, 0.0, 0.5, 1.0,
+);
+
+fn generate_matrix(aspect_ratio: f32) -> cgmath::Matrix4<f32> {
+    let mx_projection = cgmath::perspective(cgmath::Deg(45f32), aspect_ratio, 1.0, 10.0);
+    let mx_view = cgmath::Matrix4::look_at(
+        cgmath::Point3::new(1.5f32, -5.0, 3.0),
+        cgmath::Point3::new(0f32, 0.0, 1.5),
+        cgmath::Vector3::unit_z(),
+    );
+    let mx_correction = OPENGL_TO_WGPU_MATRIX;
+    mx_correction * mx_projection * mx_view
+}
+
+fn main() {
+    use winit::{
+        event,
+        event_loop::{ControlFlow, EventLoop},
+    };
+
+    env_logger::init();
+    let event_loop = EventLoop::new();
+
+    #[cfg(not(feature = "gl"))]
+    let (window, size, surface) = {
+        let window = winit::window::Window::new(&event_loop).unwrap();
+        let size = window.inner_size();
+        let surface = wgpu::Surface::create(&window);
+        (window, size, surface)
+    };
+
+    #[cfg(feature = "gl")]
+    let (window, instance, size, surface) = {
+        let wb = winit::WindowBuilder::new();
+        let cb = wgpu::glutin::ContextBuilder::new().with_vsync(true);
+        let context = cb.build_windowed(wb, &event_loop).unwrap();
+
+        let size = context
+            .window()
+            .get_inner_size()
+            .unwrap()
+            .to_physical(context.window().get_hidpi_factor());
+
+        let (context, window) = unsafe { context.make_current().unwrap().split() };
+
+        let instance = wgpu::Instance::new(context);
+        let surface = instance.get_surface();
+
+        (window, instance, size, surface)
+    };
+
+    let adapter = wgpu::Adapter::request(
+        &wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::Default,
+        },
+        wgpu::BackendBit::PRIMARY,
+    )
+    .unwrap();
+
+    let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+        extensions: wgpu::Extensions {
+            anisotropic_filtering: false,
+        },
+        limits: wgpu::Limits::default(),
+    });
+
+    // read vertex shader from shader.vert
+    let vs_module = device.create_shader_module(&wgpu::read_spirv(
+	glsl_to_spirv::compile(include_str!("shader.vsh"),
+			       glsl_to_spirv::ShaderType::Vertex).unwrap()).unwrap());
+
+    // read fragment shader from shader.frag
+    let fs_module = device.create_shader_module(&wgpu::read_spirv(
+	glsl_to_spirv::compile(include_str!("shader.fsh"),
+			       glsl_to_spirv::ShaderType::Fragment).unwrap()).unwrap());
+
+
+	// VERTEX buffer
+    let vertex_size = std::mem::size_of::<teapot::Vertex>();
+	let (vertex_data, index_data) = teapot::create_vertices();
+    let vertex_buf = device.create_buffer_with_data(vertex_data.as_bytes(), wgpu::BufferUsage::VERTEX);
+
+	// INDEX buffer
+	let index_count = vertex_data.len();
+    let index_buf = device.create_buffer_with_data(index_data.as_bytes(), wgpu::BufferUsage::INDEX);
+
+	// u_Transform
+	let aspect_ratio = 1.;
+	let mx_total = generate_matrix(aspect_ratio);
+	let mx_ref: &[f32; 16] = mx_total.as_ref();
+	let uniform_buf = device.create_buffer_with_data(mx_ref.as_bytes(),
+		wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+	);
+
+	// Bind uniform_buf
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        bindings: &[
+            wgpu::BindGroupLayoutBinding {
+                binding: 0,
+                visibility: wgpu::ShaderStage::VERTEX,
+                ty: wgpu::BindingType::UniformBuffer { dynamic: false },
+            }
+        ],
+    });
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &bind_group_layout,
+        bindings: &[
+		    wgpu::Binding {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer {
+                    buffer: &uniform_buf,
+                    range: 0 .. 64,
+                },
+            },
+		],
+    });
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        bind_group_layouts: &[&bind_group_layout],
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        layout: &pipeline_layout,
+        vertex_stage: wgpu::ProgrammableStageDescriptor {
+            module: &vs_module,
+            entry_point: "main",
+        },
+        fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+            module: &fs_module,
+            entry_point: "main",
+        }),
+        rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: wgpu::CullMode::None,
+            depth_bias: 0,
+            depth_bias_slope_scale: 0.0,
+            depth_bias_clamp: 0.0,
+        }),
+        primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+        color_states: &[wgpu::ColorStateDescriptor {
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            color_blend: wgpu::BlendDescriptor::REPLACE,
+            alpha_blend: wgpu::BlendDescriptor::REPLACE,
+            write_mask: wgpu::ColorWrite::ALL,
+        }],
+        depth_stencil_state: None,
+        index_format: wgpu::IndexFormat::Uint16,
+        vertex_buffers: &[wgpu::VertexBufferDescriptor {
+                stride: vertex_size as wgpu::BufferAddress,
+                step_mode: wgpu::InputStepMode::Vertex,
+                attributes: &[
+                    wgpu::VertexAttributeDescriptor {
+                        format: wgpu::VertexFormat::Float4,
+                        offset: 0,
+                        shader_location: 0,
+                    },
+                ],
+        }],
+        sample_count: 1,
+        sample_mask: !0,
+        alpha_to_coverage_enabled: false,
+    });
+
+    let mut sc_desc = wgpu::SwapChainDescriptor {
+        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+        format: wgpu::TextureFormat::Bgra8UnormSrgb,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Vsync,
+    };
+
+    let mut swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            event::Event::MainEventsCleared => window.request_redraw(),
+            event::Event::WindowEvent { event: event::WindowEvent::Resized(size), .. } => {
+                sc_desc.width = size.width;
+                sc_desc.height = size.height;
+                swap_chain = device.create_swap_chain(&surface, &sc_desc);
+            }
+            event::Event::RedrawRequested(_) => {
+                let frame = swap_chain
+                    .get_next_texture()
+                    .expect("Timeout when acquiring next swap chain texture");
+                let mut encoder =
+                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
+                {
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                            attachment: &frame.view,
+                            resolve_target: None,
+                            load_op: wgpu::LoadOp::Clear,
+                            store_op: wgpu::StoreOp::Store,
+                            clear_color: wgpu::Color::GREEN,
+                        }],
+                        depth_stencil_attachment: None,
+                    });
+                    rpass.set_pipeline(&render_pipeline);
+                    rpass.set_bind_group(0, &bind_group, &[]);
+					rpass.set_index_buffer(&index_buf, 0);
+                    rpass.set_vertex_buffers(0, &[(&vertex_buf, 0)]);
+					rpass.draw_indexed(0 .. index_count as u32, 0, 0 .. 1);
+                }
+
+                queue.submit(&[encoder.finish()]);
+            }
+            event::Event::WindowEvent {
+                event: event::WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
+}
---
+++ src/shader.fsh
@@ -0,0 +1,7 @@
+#version 450
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(1.0, 0.25, 0.5, 1.0);
+}
