dd979a5f0fda5fe255c9dff05c2dd7666977dcdc
Mike Garrity <mike@rustytriangles.com>
2020-02-13 07:32:30 -0500

    Run fmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 #[path = "teapot.rs"]
 mod teapot;
 
-use zerocopy::{AsBytes};
+use zerocopy::AsBytes;
 
 const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0,-1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.0,
-    0.0, 0.0, 0.5, 1.0,
+    1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.5, 1.0,
 );
 
 fn generate_matrix(aspect_ratio: f32) -> cgmath::Matrix4<f32> {
@@ -63,7 +60,8 @@ fn main() {
             power_preference: wgpu::PowerPreference::Default,
         },
         wgpu::BackendBit::PRIMARY,
-    ).unwrap();
+    )
+    .unwrap();
 
     let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
         extensions: wgpu::Extensions {
@@ -73,26 +71,46 @@ fn main() {
     });
 
     // read vertex shader from shader.vert
-    let vs_module = device.create_shader_module(&wgpu::read_spirv(
-        glsl_to_spirv::compile(include_str!("shader.vsh"),
-                               glsl_to_spirv::ShaderType::Vertex).unwrap()).unwrap());
+    let vs_module = device.create_shader_module(
+        &wgpu::read_spirv(
+            glsl_to_spirv::compile(
+                include_str!("shader.vsh"),
+                glsl_to_spirv::ShaderType::Vertex,
+            )
+            .unwrap(),
+        )
+        .unwrap(),
+    );
 
     // read fragment shader from shader.frag
-    let fs_module = device.create_shader_module(&wgpu::read_spirv(
-        glsl_to_spirv::compile(include_str!("shader.fsh"),
-                               glsl_to_spirv::ShaderType::Fragment).unwrap()).unwrap());
+    let fs_module = device.create_shader_module(
+        &wgpu::read_spirv(
+            glsl_to_spirv::compile(
+                include_str!("shader.fsh"),
+                glsl_to_spirv::ShaderType::Fragment,
+            )
+            .unwrap(),
+        )
+        .unwrap(),
+    );
 
     // get teapot coordinates
     let (vertex_data, normal_data, uv_data, index_data) = teapot::create_vertices(32, 32);
-    println!("num verts = {0}, num indices = {1}", vertex_data.len(), index_data.len());
+    println!(
+        "num verts = {0}, num indices = {1}",
+        vertex_data.len(),
+        index_data.len()
+    );
 
     // VERTEX buffer
     let vertex_size = std::mem::size_of::<teapot::Vertex>();
-    let vertex_buf = device.create_buffer_with_data(vertex_data.as_bytes(), wgpu::BufferUsage::VERTEX);
+    let vertex_buf =
+        device.create_buffer_with_data(vertex_data.as_bytes(), wgpu::BufferUsage::VERTEX);
 
     // NORMAL buffer
     let normal_size = std::mem::size_of::<[f32; 3]>();
-    let normal_buf = device.create_buffer_with_data(normal_data.as_bytes(), wgpu::BufferUsage::VERTEX);
+    let normal_buf =
+        device.create_buffer_with_data(normal_data.as_bytes(), wgpu::BufferUsage::VERTEX);
 
     // UV buffer
     let uv_size = std::mem::size_of::<[f32; 2]>();
@@ -106,31 +124,28 @@ fn main() {
     let aspect_ratio = 1.;
     let mx_total = generate_matrix(aspect_ratio);
     let mx_ref: &[f32; 16] = mx_total.as_ref();
-    let uniform_buf = device.create_buffer_with_data(mx_ref.as_bytes(),
+    let uniform_buf = device.create_buffer_with_data(
+        mx_ref.as_bytes(),
         wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
     );
 
     // Bind uniform_buf
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        bindings: &[
-            wgpu::BindGroupLayoutBinding {
-                binding: 0,
-                visibility: wgpu::ShaderStage::VERTEX,
-                ty: wgpu::BindingType::UniformBuffer { dynamic: false },
-            }
-        ],
+        bindings: &[wgpu::BindGroupLayoutBinding {
+            binding: 0,
+            visibility: wgpu::ShaderStage::VERTEX,
+            ty: wgpu::BindingType::UniformBuffer { dynamic: false },
+        }],
     });
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         layout: &bind_group_layout,
-        bindings: &[
-                wgpu::Binding {
-                    binding: 0,
-                    resource: wgpu::BindingResource::Buffer {
-                    buffer: &uniform_buf,
-                    range: 0 .. 64,
-                },
+        bindings: &[wgpu::Binding {
+            binding: 0,
+            resource: wgpu::BindingResource::Buffer {
+                buffer: &uniform_buf,
+                range: 0..64,
             },
-        ],
+        }],
     });
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         bind_group_layouts: &[&bind_group_layout],
@@ -170,44 +185,41 @@ fn main() {
             stencil_write_mask: 0,
         }),
         index_format: wgpu::IndexFormat::Uint32,
-        vertex_buffers: 
-                // position [x,y,z,w] f32
-                &[wgpu::VertexBufferDescriptor {
-                    stride: vertex_size as wgpu::BufferAddress,
-                    step_mode: wgpu::InputStepMode::Vertex,
-                    attributes: &[
-                        wgpu::VertexAttributeDescriptor {
-                            format: wgpu::VertexFormat::Float4,
-                            offset: 0,
-                            shader_location: 0,
-                        },
-                     ]},
-                 // normal [x,y,z] f32
-                 wgpu::VertexBufferDescriptor {
-                    stride: normal_size as wgpu::BufferAddress,
-                    step_mode: wgpu::InputStepMode::Vertex,
-                    attributes: &[
-                        wgpu::VertexAttributeDescriptor {
-                            format: wgpu::VertexFormat::Float3,
-                            offset: 0,
-                            shader_location: 1,
-                        },
-                    ]},
-                  // uv [u,v] f32
-                 wgpu::VertexBufferDescriptor {
-                    stride: uv_size as wgpu::BufferAddress,
-                    step_mode: wgpu::InputStepMode::Vertex,
-                    attributes: &[
-                        wgpu::VertexAttributeDescriptor {
-                            format: wgpu::VertexFormat::Float2,
-                            offset: 0,
-                            shader_location: 2,
-                        },
-                    ]},
-                ],
-            sample_count: 1,
-            sample_mask: !0,
-            alpha_to_coverage_enabled: false,
+        vertex_buffers: &[
+            // position [x,y,z,w] f32
+            wgpu::VertexBufferDescriptor {
+                stride: vertex_size as wgpu::BufferAddress,
+                step_mode: wgpu::InputStepMode::Vertex,
+                attributes: &[wgpu::VertexAttributeDescriptor {
+                    format: wgpu::VertexFormat::Float4,
+                    offset: 0,
+                    shader_location: 0,
+                }],
+            },
+            // normal [x,y,z] f32
+            wgpu::VertexBufferDescriptor {
+                stride: normal_size as wgpu::BufferAddress,
+                step_mode: wgpu::InputStepMode::Vertex,
+                attributes: &[wgpu::VertexAttributeDescriptor {
+                    format: wgpu::VertexFormat::Float3,
+                    offset: 0,
+                    shader_location: 1,
+                }],
+            },
+            // uv [u,v] f32
+            wgpu::VertexBufferDescriptor {
+                stride: uv_size as wgpu::BufferAddress,
+                step_mode: wgpu::InputStepMode::Vertex,
+                attributes: &[wgpu::VertexAttributeDescriptor {
+                    format: wgpu::VertexFormat::Float2,
+                    offset: 0,
+                    shader_location: 2,
+                }],
+            },
+        ],
+        sample_count: 1,
+        sample_mask: !0,
+        alpha_to_coverage_enabled: false,
     });
 
     let mut sc_desc = wgpu::SwapChainDescriptor {
@@ -220,44 +232,49 @@ fn main() {
 
     let mut swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
-    let mut depth_texture = device.create_texture(&wgpu::TextureDescriptor {
-        size: wgpu::Extent3d {
-            width: sc_desc.width,
-            height: sc_desc.height,
-            depth: 1,
-        },
-        array_layer_count: 1,
-        mip_level_count: 1,
-        sample_count: 1,
-        dimension: wgpu::TextureDimension::D2,
-        format: wgpu::TextureFormat::Depth32Float,
-        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-    }).create_default_view();
+    let mut depth_texture = device
+        .create_texture(&wgpu::TextureDescriptor {
+            size: wgpu::Extent3d {
+                width: sc_desc.width,
+                height: sc_desc.height,
+                depth: 1,
+            },
+            array_layer_count: 1,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Depth32Float,
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+        })
+        .create_default_view();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
             event::Event::MainEventsCleared => window.request_redraw(),
-            event::Event::WindowEvent { event: event::WindowEvent::Resized(size), .. } => {
+            event::Event::WindowEvent {
+                event: event::WindowEvent::Resized(size),
+                ..
+            } => {
                 sc_desc.width = size.width;
                 sc_desc.height = size.height;
                 swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
-                depth_texture = device.create_texture(&wgpu::TextureDescriptor {
-                    size: wgpu::Extent3d {
-                        width: sc_desc.width,
-                        height: sc_desc.height,
-                        depth: 1,
-                    },
-                    array_layer_count: 1,
-                    mip_level_count: 1,
-                    sample_count: 1,
-                    dimension: wgpu::TextureDimension::D2,
-                    format: wgpu::TextureFormat::Depth32Float,
-                    usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-                }).create_default_view();
-
-
+                depth_texture = device
+                    .create_texture(&wgpu::TextureDescriptor {
+                        size: wgpu::Extent3d {
+                            width: sc_desc.width,
+                            height: sc_desc.height,
+                            depth: 1,
+                        },
+                        array_layer_count: 1,
+                        mip_level_count: 1,
+                        sample_count: 1,
+                        dimension: wgpu::TextureDimension::D2,
+                        format: wgpu::TextureFormat::Depth32Float,
+                        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                    })
+                    .create_default_view();
             }
             event::Event::RedrawRequested(_) => {
                 let frame = swap_chain
@@ -274,21 +291,24 @@ fn main() {
                             store_op: wgpu::StoreOp::Store,
                             clear_color: wgpu::Color::BLACK,
                         }],
-                        depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachmentDescriptor {
-                            attachment: &depth_texture,
-                            depth_load_op: wgpu::LoadOp::Clear,
-                            depth_store_op: wgpu::StoreOp::Store,
-                            stencil_load_op: wgpu::LoadOp::Clear,
-                            stencil_store_op: wgpu::StoreOp::Store,
-                            clear_depth: 1.0,
-                            clear_stencil: 0,
-                        }),
+                        depth_stencil_attachment: Some(
+                            wgpu::RenderPassDepthStencilAttachmentDescriptor {
+                                attachment: &depth_texture,
+                                depth_load_op: wgpu::LoadOp::Clear,
+                                depth_store_op: wgpu::StoreOp::Store,
+                                stencil_load_op: wgpu::LoadOp::Clear,
+                                stencil_store_op: wgpu::StoreOp::Store,
+                                clear_depth: 1.0,
+                                clear_stencil: 0,
+                            },
+                        ),
                     });
                     rpass.set_pipeline(&render_pipeline);
                     rpass.set_bind_group(0, &bind_group, &[]);
-                                        rpass.set_index_buffer(&index_buf, 0);
-                    rpass.set_vertex_buffers(0, &[(&vertex_buf, 0), (&normal_buf, 0), (&uv_buf, 0)]);
-                                        rpass.draw_indexed(0 .. index_count as u32, 0, 0 .. 1);
+                    rpass.set_index_buffer(&index_buf, 0);
+                    rpass
+                        .set_vertex_buffers(0, &[(&vertex_buf, 0), (&normal_buf, 0), (&uv_buf, 0)]);
+                    rpass.draw_indexed(0..index_count as u32, 0, 0..1);
                 }
 
                 queue.submit(&[encoder.finish()]);
